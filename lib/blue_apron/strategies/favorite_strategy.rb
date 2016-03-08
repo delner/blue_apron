a8f82a595c48c3b93f40e3a8e0d44d7e1b05f595
David Elner <david@davidelner.com>
2016-03-08 17:53:26 -0500

    Added: Favorite strategy implementation and spec

--- lib/blue_apron/strategies/favorite_strategy.rb
+++ lib/blue_apron/strategies/favorite_strategy.rb
@@ -1,4 +1,22 @@
-class BlueApron::Strategies::FavoriteStrategy
+class BlueApron::Strategies::FavoriteStrategy < BlueApron::Strategy
+  def name
+    "favorite"
+  end
+  def next_move(state)
+    if most_common_move = self.most_common_move(state)
+      most_common_move.beaten_by.new
+    else
+      super
+    end
+  end
+  def most_common_move(state)
+    turns_by_player_move = state.turns.group_by { |t| t.player_move.class }
+    if turns_by_player_move.count > 0
+      most_common_move_class = turns_by_player_move.max_by { |move, turns| turns.length }.first.new
+    else
+      nil
+    end
+  end
 end
 
 # Register the strategy
--- spec/blue_apron/strategies/favorite_strategy_spec.rb
+++ spec/blue_apron/strategies/favorite_strategy_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe BlueApron::Strategies::FavoriteStrategy do
-  it { expect(BlueApron::Strategies::FavoriteStrategy).to_not be_nil }
+  let(:strategy) { BlueApron::Strategies::FavoriteStrategy.new }
+  subject { strategy }
+
+  describe "#next_move" do
+    subject { strategy.next_move(state) }
+    let(:state) { BlueApron::State.new }
+
+    context "when the state is empty (first turn)" do
+      # It isn't expecting any kind of particular move.
+      it { expect(subject).to be_a_kind_of(BlueApron::Move) }
+    end
+    context "when there is a previous move" do
+      # We really should use some test move classes, but I'll just use RPS for now
+      let(:previous_move) { BlueApron::Moves::Paper }
+      before(:each) do
+        turn = BlueApron::Turn.new
+        turn.player_move = previous_move.new
+        turn.computer_move = previous_move.new
+        state.apply_turn(turn, :tie)
+      end
+      it { expect(subject).to be_a_kind_of(BlueApron::Moves::Scissors) }
+    end
+    context "when there are several previous moves" do
+      # We really should use some test move classes, but I'll just use RPS for now
+      let(:most_common_move) { BlueApron::Moves::Scissors }
+      let(:least_common_move) { BlueApron::Moves::Rock }
+      before(:each) do
+        [{ move: most_common_move, times: 4 }, { move: least_common_move, times: 2}].each do |move_group|
+          move_group[:times].times do
+            turn = BlueApron::Turn.new
+            turn.player_move = move_group[:move].new
+            turn.computer_move = move_group[:move].new
+            state.apply_turn(turn, :tie)
+          end
+        end
+      end
+      it { expect(subject).to be_a_kind_of(BlueApron::Moves::Rock) }
+    end
+  end
 end
