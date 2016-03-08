9fecf645bb310606cf7b5b83861d425d27a1dca6
David Elner <david@davidelner.com>
2016-03-08 17:52:02 -0500

    Added: #next_move to Strategy

--- lib/blue_apron/strategy.rb
+++ lib/blue_apron/strategy.rb
@@ -1,2 +1,13 @@
 class BlueApron::Strategy
+  def name
+    "random"
+  end
+  def next_move(state)
+    self.random_move
+  end
+  def random_move
+    choices = BlueApron::Moves.all.keys
+    random_choice = [*(Range.new(0,choices.length-1))].sample
+    BlueApron::Moves.all[choices[random_choice]].new
+  end
 end
--- spec/blue_apron/strategy_spec.rb
+++ spec/blue_apron/strategy_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe BlueApron::Strategy do
-  it { expect(BlueApron::Strategy).to_not be_nil }
+  let(:strategy) { BlueApron::Strategy.new }
+  subject { strategy }
+
+  describe "#next_move" do
+    subject { strategy.next_move(state) }
+    let(:state) { BlueApron::State.new }
+
+    context "when the state is empty (first turn)" do
+      # It isn't expecting any kind of particular move.
+      it { expect(subject).to be_a_kind_of(BlueApron::Move) }
+    end
+  end
 end
