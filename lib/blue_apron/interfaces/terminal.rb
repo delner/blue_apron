650962c6c56350a68d5addc310c3f098152fd11b
David Elner <david@davidelner.com>
2016-03-08 17:55:03 -0500

    Added: Terminal input and output

--- lib/blue_apron/interfaces/terminal.rb
+++ lib/blue_apron/interfaces/terminal.rb
@@ -1,4 +1,23 @@
 module BlueApron::Interfaces
   class Terminal
+    def print_intro(info = {})
+      puts "You are playing against strategy '#{info[:strategy].name}'."
+    end
+    def print_turn(info = {})
+      outcome_text = "You win" if info[:outcome] == :win
+      outcome_text = "You lose" if info[:outcome] == :loss
+      outcome_text = "We tie" if info[:outcome] == :tie
+      puts "I chose '#{info[:turn].computer_move.class.move_name}'. #{outcome_text}!"
+    end
+    def print_record(info = {})
+      puts "you won #{info[:record][:win]} times."
+      puts "you lost #{info[:record][:loss]} times."
+      puts "we tied #{info[:record][:tie]} times."
+    end
+    def prompt_move(info = {})
+      puts "Type 'r', 'p', 's' or 'x'."
+      print "> "
+      STDIN.gets.chomp
+    end
   end
 end
