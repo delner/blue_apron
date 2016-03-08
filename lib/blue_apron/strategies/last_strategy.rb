7691a44304640eab60c055ad7d91d1c738ed2cec
David Elner <david@davidelner.com>
2016-03-08 17:53:14 -0500

    Added: Last strategy implementation and spec

--- lib/blue_apron/strategies/last_strategy.rb
+++ lib/blue_apron/strategies/last_strategy.rb
@@ -1,4 +1,14 @@
-class BlueApron::Strategies::LastStrategy
+class BlueApron::Strategies::LastStrategy < BlueApron::Strategy
+  def name
+    "last"
+  end
+  def next_move(state)
+    if last_turn = state.turns.last
+      last_turn.player_move.beaten_by.new
+    else
+      super
+    end
+  end
 end
 
 # Register the strategy
